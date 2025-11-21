0f5fc1a99e16504c9d021af2b360588b72858ff3
hardwareowen-cmd <hardwareowen@gmail.com>
2025-11-21 16:22:58 +0000

    Added files and content

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/boys')
+def boys():
+    return render_template("boys.html")
+
+@app.route('/girls')
+def girls():
+    return render_template("girls.html")
 if __name__ == '__main__':
     app.run()
